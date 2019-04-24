4d26dd2995939dd37b780fc3b0ae618706ed4f7d
github username <github email address>
2019-04-24 08:52:09 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+    
+    def name=(name)
+        @name = name
+    end
+    # attr_reader = :name
+
+
+    def name
+        @name
+    end
+
+    # attr_writer = :name
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+end
