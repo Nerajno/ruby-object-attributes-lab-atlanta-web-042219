5338e9ae492a4c78c6e2fcaed119fbcb4431f850
github username <github email address>
2019-05-22 17:42:38 -0400

    round_2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,19 @@
 class Dog
     
     def name=(name)
-        @name = name
+        @name= name
     end
-    # attr_reader = :name
-
 
     def name
         @name
     end
 
-    # attr_writer = :name
     def breed=(breed)
-        @breed = breed
+        @breed= breed
     end
 
     def breed
         @breed
     end
+
 end
